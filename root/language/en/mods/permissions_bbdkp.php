1b5787697ffaa64049484f5461040980e5c57ee6
Andy Vandenberghe <Sajaki9@gmail.com>
2010-07-14 22:57:34 +0000

    v1.1.1 [new] user permissions

--- root/language/en/mods/permissions_bbdkp.php
+++ root/language/en/mods/permissions_bbdkp.php
@@ -32,8 +32,8 @@ $lang['permission_type']['bbdkp_'] = 'bbDkp Permissions';
 
 // bbDkp Permissions
 $lang = array_merge($lang, array(
-	'acl_a_dkp'		=> array('lang' => 'bbDkp - can add/edit dkp', 'cat' => 'bbdkp'),
-	'acl_a_dkp_no'	=> array('lang' => 'bbDkp - can edit dkp settings', 'cat' => 'bbdkp'),
+	'acl_a_dkp'		=> array('lang' => 'bbDkp - can access bbDKP ACP', 'cat' => 'bbdkp'),
+	'acl_u_dkp'		=> array('lang' => 'bbDkp - can see DKP pages', 'cat' => 'bbdkp'),
 ));
 
 ?>
