8bd31ec207810f8f5425789378096a37273d32df
Andy Vandenberghe <sajaki9@gmail.com>
2011-03-14 20:18:26 +0100

    French translation update

--- root/language/fr/mods/info_acp_dkp_raid.php
+++ root/language/fr/mods/info_acp_dkp_raid.php
@@ -1,33 +1,46 @@
 <?php
 /**
- * bbdkp acp language file for Raid menu (FR)
- * 
- * @package bbDkp
- * @copyright 2009 bbdkp <http://code.google.com/p/bbdkp/>
- * @license http://opensource.org/licenses/gpl-license.php GNU Public License
- * @version $Id$
- * 
- */
+*
+* mods_info_acp_dkp_raid.php [French]
+*
+* @package language
+* @version $Id: $
+* @copyright (c) 2011 phpBB Group
+* @author 2011-03-14 - Maël Soucaze
+* @license http://opensource.org/licenses/gpl-license.php GNU Public License
+*
+*/
 
 /**
-* @ignore
+* DO NOT CHANGE
 */
 if (!defined('IN_PHPBB'))
 {
 	exit;
 }
 
-// Create the lang array if it does not already exist
 if (empty($lang) || !is_array($lang))
 {
 	$lang = array();
 }
 
-// Merge the following language entries into the lang array
+// DEVELOPERS PLEASE NOTE
+//
+// All language files should use UTF-8 as their encoding and the files must not contain a BOM.
+//
+// Placeholders can now contain order information, e.g. instead of
+// 'Page %s of %s' you can (and should) write 'Page %1$s of %2$s', this allows
+// translators to re-order the output of data while ensuring it remains correct
+//
+// You do not need this where single placeholders are used, e.g. 'Message %d' is fine
+// equally where a string contains only two placeholders which are used to wrap text
+// in a url you again do not need to specify an order e.g., 'Click %sHERE%s' is fine
+
 $lang = array_merge($lang, array(
-	'ACP_DKP_RAIDS'		    => 'Gestion des Raids',  
-	'ACP_DKP_RAID_ADD'		=> 'Ajouter Raid manuellement',
-	'ACP_DKP_RAID_LIST'		=> 'Liste des Raids',
+	'ACP_DKP_RAID_EDIT'	=> 'Edition Raid',
+	'ACP_DKP_RAIDS'	=> 'Gestion des Raids',
+	'ACP_DKP_RAID_ADD'	=> 'Ajouter Raid manuellement',
+	'ACP_DKP_RAID_LIST'	=> 'Liste des Raids',
 ));
 
 ?>
