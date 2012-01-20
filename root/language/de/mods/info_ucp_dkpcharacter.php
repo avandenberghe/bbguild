f7c299bba8b29363a5cfc32d44d6cfa515a79990
killerpommes <killerpommes88@web.de>
2012-01-20 10:50:19 +0100

    fixed translation

--- root/language/de/mods/info_ucp_dkpcharacter.php
+++ root/language/de/mods/info_ucp_dkpcharacter.php
@@ -26,7 +26,7 @@ if (empty($lang) || !is_array($lang))
 
 // Merge the following language entries into the lang array
 $lang = array_merge($lang, array(
-    'UCP_DKP_CHARACTERS'	=> 'Charactere',
+    'UCP_DKP_CHARACTERS'	=> 'Charaktere',
 	'UCP_DKP'				=> 'bbDKP Benutzermenü',  
 	'UCP_DKP_CHARACTER_LIST'	=> 'Meine Charaktere',
 	'UCP_DKP_CHARACTER_ADD'		=> 'Charakter hinzufügen',
