d3f61ac663d4664bbe0c72600658d5a052f1ad2d
Sajaki <sajaki9@gmail.com>
2012-01-31 00:37:18 +0100

    modx update script 125PL3 -> 126 is done

--- root/configdkp.php
+++ root/configdkp.php
@@ -1,7 +1,7 @@
 <?php
 /* @package bbDKP
  * @license http://opensource.org/licenses/gpl-license.php GNU Public License
- * bbDKP 1.2.2 configuration file
+ * bbDKP 1.2.6 configuration file
 */ 
 
 /**
