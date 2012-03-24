eafbd16cf3fb98c6077f73f9da018abbe2577759
Sajaki <sajaki9@gmail.com>
2012-03-24 19:48:32 +0100

    cleanup..

--- root/includes/acp/acp_dkp_mdkp.php
+++ root/includes/acp/acp_dkp_mdkp.php
@@ -265,8 +265,6 @@ class acp_dkp_mdkp extends bbDKP_Admin
 						
 					$template->assign_block_vars('members_row', $members_row);
 					
-					//$previous_data = $row[$previous_source];
-					
 					// unset array 
 					unset($members_row);
 				}
