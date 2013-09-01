3c740bc6a0bcc415b235379194be228d5a11d675
Andreas <sajaki9@gmail.com>
2013-09-01 14:45:46 +0200

    fix install.xml, re-add info file for backwards compatibility

---
+++ root/includes/acp/info/acp_dkp_event.php
@@ -0,0 +1,46 @@
+<?php
+/**
+ * @version 1.3.0
+ * @copyright (c) 2009 bbdkp https://github.com/bbDKP
+ * @license http://opensource.org/licenses/gpl-license.php GNU Public License
+ * @package acp
+ * @deprecated file deprecated in rel 1.3
+ * @note : only needed for uninstallation of events module!
+ /
+
+/**
+* @ignore
+*/
+if (!defined('IN_PHPBB'))
+{
+	exit;
+}
+
+/**
+ * @package acp
+ */
+class acp_dkp_event_info
+{
+	function module()
+	{
+		return array(
+			'filename'	=> 'acp_dkp_event',
+			'title'		=> 'ACP_DKP_EVENT',
+			'version'	=> '1.3.0',
+			'modes'		=> array(
+				'addevent'			=> array('title' => 'ACP_DKP_EVENT_ADD', 'auth' => 'acl_a_dkp', 'cat' => array('ACP_DKP_EVENT') , 'display' => false),
+				'listevents'		=> array('title' => 'ACP_DKP_EVENT_LIST', 'auth' => 'acl_a_dkp', 'cat' => array('ACP_DKP_EVENT')),
+				),
+		);
+	}
+
+	function install()
+	{
+	}
+
+	function uninstall()
+	{
+	}
+}
+
+?>
