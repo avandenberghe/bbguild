a382e9e7c63f9c1a56d0c2467f9717f33d8e90a6
Andreas <sajaki9@gmail.com>
2014-01-04 14:51:21 +0100

    portal blocks update prosilver

--- root/portal.php
+++ root/portal.php
@@ -19,6 +19,7 @@ define('IN_BBDKP', true);
 $phpbb_root_path = (defined('PHPBB_ROOT_PATH')) ? PHPBB_ROOT_PATH : './';
 $phpEx = substr(strrchr(__FILE__, '.'), 1);
 include($phpbb_root_path . 'common.' . $phpEx);
+include($phpbb_root_path . 'includes/functions_display.' . $phpEx);
 
 $user->session_begin();
 $auth->acl($user->data);
@@ -47,7 +48,7 @@ $template->assign_var('S_PORTAL', true);
 page_header($user->lang['NEWS']);
 
 $template->set_filenames(array(
-		'body' => 'dkp/news_body.html')
+		'body' => 'dkp/portal_body.html')
 );
 
 $views = new \bbdkp\views\views();
