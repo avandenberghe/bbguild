8ff2ab32e56b5837bc62470d84409da5b4bc9d11
Andreas <sajaki9@gmail.com>
2015-09-12 17:46:40 +0200

    fix blockfactory. now points to correct raid block class

--- root/includes/bbdkp/block/blockFactory.php
+++ root/includes/bbdkp/block/blockFactory.php
@@ -5,7 +5,7 @@
  * @author Sajaki@gmail.com
  * @copyright 2009 bbdkp
  * @license http://opensource.org/licenses/gpl-license.php GNU Public License
- * @version 1.4.1
+ * @version 1.3.0
  *
  */
 namespace bbdkp\views;
@@ -73,12 +73,12 @@ class blockFactory extends \bbdkp\admin\Admin
             if ($config['rp_show_portal'] == 1)
             {
                 $user->add_lang(array('mods/raidplanner'));
-                if (!class_exists('\bbdkp\views\raidplanner\rpblocks', false))
+                if (!class_exists('\bbdkp\raidplanner\rpblocks', false))
                 {
                     //display the blocks
-                    include($phpbb_root_path . 'includes/bbdkp/block/Rpblocks.' . $phpEx);
+                    include($phpbb_root_path . 'includes/bbdkp/block/rpblocks.' . $phpEx);
                 }
-                $blocks = new \bbdkp\views\raidplanner\rpblocks();
+                $blocks = new \bbdkp\raidplanner\rpblocks();
                 $blocks->display();
             }
         }
@@ -88,3 +88,4 @@ class blockFactory extends \bbdkp\admin\Admin
 
 
 }
+
