26a8ae778d143efa98e187bf4680407e8d9248bb
Sajaki <Sajaki9@gmail.com>
2016-05-12 18:51:05 +0200

    block classes

---
+++ model/blocks/newsblock.php
@@ -0,0 +1,15 @@
+<?php
+/**
+ * new block class
+ *
+ * @package   bbguild v2.0
+ * @copyright 2016 bbDKP <https://github.com/bbDKP>
+ * @license   http://opensource.org/licenses/gpl-2.0.php GNU General Public License v2
+ *
+ */
+namespace bbdkp\bbguild;
+
+class newsblock
+{
+	
+}
